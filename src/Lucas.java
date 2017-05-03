81cfddd65965a2a997d660ff0a06e6af9c73efa7
stefan2212 <stefan.cernescu94@gmail.com>
2017-05-03 21:03:53 +0100

    Finished

--- src/Lucas.java
+++ src/Lucas.java
@@ -17,14 +17,41 @@ public class Lucas {
     }
 
     public boolean isPrime(){
-        BigInteger s=new BigInteger("4");
         int p=getP();
+        if(isPrime(p)==false)
+            return false;
+        BigInteger s=new BigInteger("4");
         int iteration=1;
         while(iteration!=p-1){
             BigInteger sPatrat=s.pow(2);
-            s=sPatrat.subtract(new BigInteger("2")).mod(n);
+            if(sPatrat.intValue()-2>n.intValue())
+                s=new BigInteger(String.valueOf(modularReduction(sPatrat.intValue()-2,p,n.intValue()))).mod(n);
+            else
+                s=sPatrat.subtract(new BigInteger("2"));
+
             iteration++;
         }
         return s.intValue()==0;
+    }
+
+    private boolean isPrime(int p)
+    {
+        for(int i=2;i<=p/2;i++)
+            if(p%i==0)
+                return false;
+        return true;
+    }
+
+    public int modularReduction(int value,int p,int n){
+        int currX1=1;
+        int prevX1=1;
+        while(currX1*Math.pow(2,p)<value)
+        {
+            prevX1=currX1;
+            currX1++;
+        }
+        int x1=prevX1;
+        int x2=value - (int) (Math.pow(2,p)*x1);
+        return x1+x2;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solovay s = new Solovay(new BigInteger("31"));
-        System.out.println(s.isPrime(1000));
+        System.out.println(s.isPrime(10));
 
 
         Lucas l=new Lucas(new BigInteger("31"));
